c3dcf45988919f1f8f29378cbd7cbc27768d0503
Kevin J Nguyen <kevinjnguyen2@gmail.com>
2021-06-07 18:34:39 -0500

    Moved up a directory

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/src'),
+      dir: require('path').join(__dirname, './coverage/simple-crm'),
       subdir: '.',
       reporters: [
         { type: 'html' },
